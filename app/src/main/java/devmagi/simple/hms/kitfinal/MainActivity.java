1fe9718ddadd4be334a1967fc928b499ed25b381
ZAHHAR-ISMAIL <zahhar.ismail@gmail.com>
2022-11-07 15:50:25 +0100

    0

---
+++ app/src/main/java/devmagi/simple/hms/kitfinal/MainActivity.java
@@ -0,0 +1,15 @@
+package devmagi.simple.hms.kitfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
